4e5536da1a6cd3be909ec41c2470ece17bcc931d
rouzwelt <rouzweltt@gmail.com>
2023-04-09 01:48:42 +0000

    ci publish workflow
    - CI publish workflow
    - update readme
    - update rainlang dep

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,9 @@
+// import path from "path";
 import ts from "rollup-plugin-ts";
 // import babel from "@rollup/plugin-babel";
+// import license from 'rollup-plugin-license';
 import { lezer } from "@lezer/generator/rollup";
+
 
 export default {
     input: "src/index.ts",
@@ -11,6 +14,24 @@ export default {
     plugins: [
         ts(),
         lezer(),
+        // license({
+        //     sourcemap: true,
+        //     cwd: process.cwd(), // The default
+        // banner: {
+        //     commentStyle: 'regular', // The default
+        //     content: {
+        //         file: path.join(__dirname, 'src/services/LICENSE'),
+        //         encoding: 'utf-8', // Default is utf-8
+        //     },
+        // },
+        // thirdParty: {
+        //     // includePrivate: true, // Default is false.
+        //     output: {
+        //         file: path.join(__dirname, 'dist', 'dependencies.txt'),
+        //         encoding: 'utf-8', // Default is utf-8.
+        //     },
+        // },
+        // }),
         // babel({
         //     babelHelpers: "bundled"
         // })
